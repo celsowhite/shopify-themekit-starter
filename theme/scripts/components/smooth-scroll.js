b49fd2d1ba4a84ff87521f6a2ba340e360a93cf7
Celso White <celso.white13@gmail.com>
2021-05-19 10:47:53 -0400

    Use gsap for smooth scroll.

--- theme/scripts/components/smooth-scroll.js
+++ theme/scripts/components/smooth-scroll.js
@@ -1,13 +1,36 @@
-import SmoothScroll from 'smooth-scroll';
+import { gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 
 /*-------------------------
 Smooth Scroll
 -------------------------*/
 
-document.addEventListener('DOMContentLoaded', event => {
-  const scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 800,
-    header: '.main-header',
-    updateURL: false,
+function initSmoothScroll() {
+  gsap.registerPlugin(ScrollToPlugin);
+  const mainHeader = document.querySelector('.main-header');
+  const mainHeaderHeight = mainHeader.offsetHeight;
+  const anchorLinks = document.querySelectorAll('a[href*="#"]');
+
+  [...anchorLinks].forEach(link => {
+    const linkHash = `#${link.href.split('#')[1]}`;
+    link.addEventListener('click', e => {
+      e.preventDefault();
+
+      gsap.to(window, {
+        duration: 1,
+        scrollTo: {
+          y: `${linkHash}`,
+          offsetY: mainHeaderHeight,
+        },
+      });
+    });
   });
+}
+
+/*-------------------------------
+Init
+-------------------------------*/
+
+document.addEventListener('DOMContentLoaded', event => {
+  initSmoothScroll();
 });
