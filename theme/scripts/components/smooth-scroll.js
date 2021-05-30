bca0ecfd3e52b9490d9ea2d6b0e619a7ad2941b3
Celso White <celso.white13@gmail.com>
2021-05-30 11:13:28 -0400

    Make smooth scroll functionality more robust.

--- theme/scripts/components/smooth-scroll.js
+++ theme/scripts/components/smooth-scroll.js
@@ -12,18 +12,24 @@ function initSmoothScroll() {
   const anchorLinks = document.querySelectorAll('a[href*="#"]');
 
   [...anchorLinks].forEach(link => {
+    // Save the link components
+    const linkUrl = `${link.href.split('#')[0]}`;
     const linkHash = `#${link.href.split('#')[1]}`;
-    link.addEventListener('click', e => {
-      e.preventDefault();
 
-      gsap.to(window, {
-        duration: 1,
-        scrollTo: {
-          y: `${linkHash}`,
-          offsetY: mainHeaderHeight,
-        },
+    // If this link is an internal link for the current page then activate the smooth scroll.
+    if (window.location.href.includes(linkUrl) && linkHash !== '#') {
+      link.addEventListener('click', e => {
+        e.preventDefault();
+
+        gsap.to(window, {
+          duration: 1,
+          scrollTo: {
+            y: `${linkHash}`,
+            offsetY: mainHeaderHeight,
+          },
+        });
       });
-    });
+    }
   });
 }
 
